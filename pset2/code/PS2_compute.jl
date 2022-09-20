a91f54c2b11939b724c039444c0b0ec0ecb4597b
AnnaLukianova <104737877+AnnaLukianova@users.noreply.github.com>
2022-09-20 16:11:10 -0500

    Add files via upload

--- pset2/code/PS2_compute.jl
+++ pset2/code/PS2_compute.jl
@@ -1,55 +1,35 @@
 using Parameters, Plots, Statistics #import the libraries we want
 include("PS2_model.jl") #import the functions that solve our growth model
 
-prim, res = Initialize() #initialize primitive and results structs
-@elapsed Solve_model(prim, res) #solve the model!
+prim, res = Initialize(0.994) #initialize primitive and results structs
+
+d = ClearMarket(prim, res)
+
 @unpack val_func, pol_func, distr, q = res
 @unpack a_grid, s_grid = prim
 
-I = Indicator(prim, res)
-mean(I)
-I[10, 11, :]
-findall(x -> x == 1, I)
+############## Make plots
+### distribution
 
-res.distr = Distr(prim, res)
-sum(res.distr)
-Plots.plot(a_grid, res.distr[:, 1], title= "Distribution of assets", label = "Good shock")
-Plots.plot!(a_grid, res.distr[:, 2], title= "Distribution of assets", label = "Bad shock")
+Plots.plot(a_grid .+ 1, res.distr[:, 1], title= "Distribution of wealth (a+s)", xlim = [-2, 3], label = "Good shock")
+Plots.plot!(a_grid .+ 0.5, res.distr[:, 2], title= "Distribution of wealth (a+s)", xlim = [-2, 3], xlabel = "Wealth", label = "Bad shock")
+Plots.savefig("PS2_Distribution.png")
 
-ED = ExcessDemand(prim, res)
-
-prim, res = Initialize(0.994) #initialize primitive and results structs
-V_iterate(prim, res)
-Distr(prim, res)
-ExcessDemand(prim, res)
-d = ClearMarket(prim, res)
-
-for q = 0.7:0.05:0.9
-    prim, res = Initialize(q)
-    V_iterate(prim, res)
-    println("pol_func[1:10, 1] = ", res.pol_func[1:10, 1], "q = $q")
-    println("pol_func[1:10, 2] = ", res.pol_func[1:10, 2], "q = $q")
-end 
-
-
-
-##############Make plots
 #value function
 Plots.plot(a_grid, val_func[:, 1], title="Value Function", label = "Good shock")
-Plots.plot!(a_grid, val_func[:, 2], title="Value Function", label = "Bad shock", legend =:bottomright) 
+Plots.plot!(a_grid, val_func[:, 2], title="Value Function", label = "Bad shock", yaxis = "Value", xaxis = "Current assets", legend =:bottomright)
+Plots.savefig("PS2_Value_Functions.png")
 
-#policy function 
+#policy function
 Plots.plot(a_grid, pol_func[:, 1], title="Policy Functions", label = "Good shock")
-Plots.plot!(a_grid, pol_func[:, 2], title="Policy Functions",  label = "Bad shock", legend =:bottomright)
+Plots.plot!(a_grid, pol_func[:, 2], title="Policy Functions",  label = "Bad shock", yaxis = "Future assets", xaxis = "Current assets", legend =:bottomright)
 plot!(a_grid, a_grid, label = "45* line", linestyle = :dash)
-Plots.savefig("PS2 _Policy_Functions.png")
+Plots.savefig("PS2_Policy_Functions.png")
 
 #changes in policy function
 pol_func_δ_1 = copy(pol_func[:, 1]).-a_grid
 pol_func_δ_2 = copy(pol_func[:, 2]).-a_grid
 
-Plots.plot(a_grid, pol_func_δ_1, title="Policy Functions Changes. Good shock")
-Plots.savefig("PS2_Policy_Functions_Changes_good.png")
-
-Plots.plot(a_grid, pol_func_δ_2, title="Policy Functions Changes. Bad shock")
-Plots.savefig("PS2_Policy_Functions_Changes_bad.png")
+Plots.plot(a_grid, pol_func_δ_1, title="Policy Functions Changes", label = "Good shock")
+Plots.plot!(a_grid, pol_func_δ_2, title="Policy Functions Changes", label = "Bad shock")
+Plots.savefig("PS2_Policy_Functions_Changes.png")
