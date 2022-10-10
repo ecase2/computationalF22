6dd6a28f28b6e0d212534c8b78a417064fa5d4a3
hhan <hhan82@wisc.edu>
2022-10-10 09:16:57 -0500

    Organized final PS4 files

--- pset4/code/ps4_init.jl
+++ pset4/code/ps4_init.jl
@@ -8,120 +8,130 @@
     *    results    - mutable struct, contains results of our model
 =#
 @with_kw struct primitives
-    # values we will not need to change
     N::Int64   = 66     # age of death
     n::Float64 = 0.011  # population growth
     R::Int64   = 46     # age of retirement
 
-    σ::Int64   = 2      # coefficient of relative risk aversion
+    σ::Float64   = 2.0      # coefficient of relative risk aversion
     α::Float64 = 0.36   # capital share
     δ::Float64 = 0.06   # depreciation rate
     β::Float64 = 0.97   # discount rate
-    γ::Float64 = 0.42   # weight on consumption
+
+    η::Array{Float64,1} = DataFrame(CSV.File(root*"/ef.csv"))[:, 1] # age-efficiency profile
 
     # transition matrix
     π_HH::Float64 = 0.9261
     π_LL::Float64 = 0.9811
     π::Matrix{Float64} = [π_HH (1-π_HH) ; (1-π_LL) π_LL] # productivity persistence probability matrix
-    π0::Matrix{Float64} = [0.2037 0.7963]                # ergodic productivity distribution
-    η::Array{Float64,1} = DataFrame(CSV.File(root*"/ef.csv"))[:, 1] # age-efficiency profile
+    π0::Matrix{Float64} = [0.2037 0.7963]
 
     # normalizing the μ distribution - initial value
     μ_1::Float64 = (n*(1+n)^(N-1)) / ((1+n)^N - 1)
 
-    z::Vector{Float64} = [3.0, 0.5]         # productivity shocks
-    e::Matrix{Float64} = η*transpose(z)     # productivity
-end
-
-mutable struct inputs   # EMILY: is "changeables" or some other name more appropriate?
-    # parameters or inputs that will change
-    θ::Float64              # proportional labor income tax (for social security)
-    T::Int64                # number of time periods
-    t_noss::Int64           # time period when no SS takes effect
-
     # assets grid
-    na::Int64   # number of points in the asset grid
-    a_grid::Array{Float64} # asset grid
-end
+    al::Int64 = 0   # lower bound for assets
+    au::Int64 = 20  # upper bound for assets - some arbitraty number
+    na::Int64 = 200 # number of points in the asset grid
+    step::Float64 = (au - al)/(na-1)
+    a_grid::Array{Float64} = collect(al:step:au)
 
-function initInputs(θ_input::Float64, T_input::Int64, al::Int64, au::Int64, na_input::Int64, t_noss::Int64)
-    θ      = θ_input
-    T      = T_input
-    t_noss = t_noss
-    na = na_input
-    a_grid = collect(range(au, al, na))
+    θ::Float64 = 0.11
+    z::Vector{Float64} = [3.0, 0.5]
+    γ::Float64 = 0.42
+    e::Array{Float64, 2} = η*transpose(z)
 
-    ins    = inputs(θ, T, t_noss, na, a_grid)
-    return ins
+    T::Int64 = 1
+    t_noss::Int64 = 2
+
 end
 
 mutable struct results
-    # results of the model
-    # value and policy functions, distribution are four-dimensional objects (assets - productivity - age - period)
-    val_func::Array{Float64, 4} # value function
-    pol_func::Array{Float64, 4} # policy function
-    Γ::Array{Float64, 4}        # distribution
-    labor::Array{Float64, 4}    # optimal labor choice
 
-    # endogenous prices, rows = t
-    w::Array{Float64, 1} # wage transition path
-    r::Array{Float64, 1} # interest rate
-    b::Array{Float64, 1} # pension benefits
-    θ_path::Array{Float64, 1}
+    # value and policy functions, distribution are three-dimensional objects (assets - productivity - age)
+    val_func::Array{Any, 3} # value function
+    pol_func::Array{Any, 3} # policy function
+    Γ::Array{Any, 3}        # distribution
+    labor::Array{Any, 3}        # optimal labor choice
+
+    # endogenous prices
+    w::Float64 # wage
+    r::Float64 # interest rate
+    b::Float64 # pension benefits
 
     # aggregates
-    K::Array{Float64, 1} # aggregate capital
-    L::Array{Float64, 1} # aggregate labor
+    K::Float64 # aggregate capital
+    L::Float64 # aggregate labor
 end
 
-include("ps4_ss_values.jl")
-function initResults(prim::primitives, ins::inputs)
-    @unpack na, T, t_noss, θ = ins
-    @unpack N     = prim
+#function for initializing model primitives and results
+function Initialize(al::Float64, au::Float64, na::Int64, θ::Float64, z::Vector{Float64}, γ::Float64, T::Int64, t_noss::Int64)
+    prim = primitives(al = al, au = au, na = na, θ = θ, z = z, γ = γ, T = T, t_noss = t_noss) #initialize primitives
 
-    w = zeros(T)
-    w[1]   = 1.335  # steady state wage with SS
-    w[end] = 1.436  # steady state wage with no SS
+    val_func = zeros(prim.na, 2, prim.N) #initial value function guess
+    pol_func = zeros(prim.na, 2, prim.N) #initial policy function guess
+    Γ        = zeros(prim.na, 2, prim.N)
+    labor    = zeros(prim.na, 2, prim.N)
 
-    r = zeros(T)
-    r[1]   = 0.037  # steady state interest rate with SS
-    r[end] = 0.026  # steady state interest rate with no SS
+    w = 1.05
+    r = 0.05
+    b = 0.2
+    if prim.θ == 0.0
+        b = 0.0
+    end
 
-    b = zeros(T)
-    b[1] = 0.231    # steady state benefits with SS
+    K = 0.0
+    L = 0.0
 
-    K = zeros(T)
-    K[1]   = 2.953  # steady state aggregate capital with SS
-    K[end] = 3.853  # steady state aggregate capital with no SS
+    res  = results(val_func, pol_func, Γ, labor, w, r, b, K, L) #initialize results struct
 
-    θ_path = ones(T)*θ
-
-    L = zeros(T)
-    L[1]   = 0.383  # steady state aggregate labor with SS
-    L[end] = 0.408  # steady state aggregate labor with no SS
-
-    val_func = zeros(T, na, 2, N)
-    pol_func = zeros(T, na, 2, N)
-    labor    = zeros(T, na, 2, N)
-    Γ = zeros(T, na, 2, N)
-
-    val_worker, pol_worker, labor[1,:,:,:] = bellman_worker(prim, ins, w[1], r[1], 0.11)
-    val_func[1, :, :, :], pol_func[1, :, :, :] = bellman_retiree(prim, ins, w[1], r[1], b[1], 0.11, val_worker, pol_worker)
-
-    Γ[1, :,:,:]   = get_ss_distr(pol_func[1, :, :, :], prim, ins)
-
-    val_worker, pol_worker, labor[end,:,:,:] = bellman_worker(prim, ins, w[end], r[end], 0.0)
-    val_func[end, :, :, :], pol_func[end, :, :, :] = bellman_retiree(prim, ins, w[end], r[end], b[end], 0.0, val_worker, pol_worker)
-    Γ[end, :,:,:] = get_ss_distr(pol_func[end, :, :, :],  prim, ins)
-
-    res = results(val_func, pol_func, Γ, labor, w, r, b, θ_path, K, L)
-    return res
+    prim, res
 end
 
-### For exercise 1 use t_noss = 2, while for exercise 2 use t_noss = 21
-function initialize(θ_input::Float64 = 0.011, T_input::Int64 = 30, al::Int64 = 0, au::Int64 = 80, na_input::Int64 = 500, t_noss::Int64 = 2)
-    prim = primitives()
-    ins  = initInputs(θ_input, T_input, al, au, na_input, t_noss)
-    res  = initResults(prim, ins)
-    return prim, ins, res
+
+mutable struct results_trans
+    # results of the model
+    val_func::Array{Float64,4}  #value function is 4D - age, z1, z2, T
+    pol_func::Array{Float64,4}
+    labor::Array{Float64,4}  #labor supply function
+    Γ::Array{Float64,4}
+
+    # PRICES AND AGGS NEED TO BE ARRAYS NOW - THEY ARE PATHS OVER TIME
+    w::Array{Float64,1} #wage path
+    r::Array{Float64,1} #interest rate path
+    b::Array{Float64,1} #SS pension benefits path
+    K::Array{Float64} #agg capital path
+    L::Array{Float64} #agg labor path
+
+    #NEW
+    θ_path::Array{Float64}
+end
+
+
+function Initialize_trans(al::Float64, au::Float64, na::Int64, θ::Float64, z::Vector{Float64}, γ::Float64, T::Int64, t_noss::Int64)
+    prim = primitives(al = al, au = au, na = na, θ = θ, z = z, γ = γ, T = T, t_noss = t_noss) #initialize primitives
+
+    val_func = zeros(prim.T, prim.na, 2, prim.N) #initial value function guess
+    pol_func = zeros(prim.T, prim.na, 2, prim.N) #initial policy function guess
+    Γ        = zeros(prim.T, prim.na, 2, prim.N)
+    labor    = zeros(prim.T, prim.na, 2, prim.N)
+
+    w = 1.05*ones(prim.T)
+    r = 0.05*ones(prim.T)
+    b = 0.2*ones(prim.T)
+
+    K = zeros(prim.T)
+    L = zeros(prim.T)
+
+    θ_path = θ*ones(prim.T)
+
+    for t = 1:T
+        if t >= t_noss
+            θ_path[t] = 0
+            b[t] = 0
+        end
+    end
+
+    res  = results_trans(val_func, pol_func, Γ, labor, w, r, b, K, L, θ_path) #initialize results struct
+
+    prim, res
 end
--- pset4/code/ps4_model.jl
+++ pset4/code/ps4_model.jl
@@ -1,55 +1,12 @@
 ### more functions here ###
 
-function initialGuesses(ins::inputs, res::results)
-    @unpack  T = ins
-    @unpack  K, L = res
-
-    # Guess for K and L - T points from K[1] (L[1]) and K[end] (L[end]) equally spaced
-    K_step = (K[end] - K[1])/(T-1)
-    L_step = (L[end] - L[1])/(T-1)
-    for i = 2:T-1
-        K[i] = K[i-1] + K_step
-        L[i] = L[i-1] + L_step
-    end
-    return K, L
-end
-K, L = initialGuesses(ins, res)
-w, r, b = prices(prim, ins, res; Kg=K, Lg=L)
-
-function prices(prim::primitives, ins::inputs, res::results; Kg::Array{Float64}, Lg::Array{Float64})
-    @unpack α, δ, R, N, n = prim
-    @unpack θ, T, t_noss = ins
-    #@unpack K, L  = res
-
-    # Path of wages w
-    for t = 1:T
-        res.w[t] = (1-α) * Kg[t]^α * Lg[t]^(-α)
-
-        # Path of interest rates r
-        res.r[t] = α * Kg[t]^(α-1) * Lg[t]^(1-α) - δ
-    end
-
-    # Path of pensions
-    for t = 1:T
-        if t < t_noss
-            @unpack w = res
-            μ = AgeDistribution(N, n)
-            res.b[t] = (θ*w[t]*L[t])/ sum(μ[R:N])
-        else
-            res.b[t] = 0
-            res.θ_path[t] = 0
-        end
-    end
-    return res.w, res.r, res.b
-end
 
 # Finds the optimal labor supply
-function getLabor(a_index, z_index, ap_index, prim::primitives, ins::inputs, t::Int64, age::Int64)
-    @unpack e, γ, R, δ = prim
-    @unpack θ, a_grid = ins
-    @unpack w, r, b = res
+function getLabor(a_index, z_index, ap_index, prim::primitives,  res::results_trans, t::Int64, age::Int64)
+    @unpack e, γ, R, δ, θ, a_grid  = prim
+    @unpack w, r, b, θ_path = res
 
-    l = (γ*(1-θ)*e[age, z_index]*w[t] - (1-γ)*((1+r[t])*a_grid[a_index] - a_grid[ap_index])) / ((1-θ)*w[t]*e[age, z_index])
+    l = (γ*(1-θ_path[t])*e[age, z_index]*w[t] - (1-γ)*((1+r[t])*a_grid[a_index] - a_grid[ap_index])) / ((1-θ_path[t])*w[t]*e[age, z_index])
 
     if l > 1.0
         l = 1.0
@@ -60,183 +17,147 @@ function getLabor(a_index, z_index, ap_index, prim::primitives, ins::inputs, t::
     return l
 end
 
-# Finds utility of retirees
-function UtilityRetiree(c::Float64, σ::Int64, γ::Float64)
-    if c > 0
-        u_r = (c^((1-σ)*γ))/(1-σ) # CRRA utility for retiree
-    else
-        u_r = -Inf
-    end
+#utility grid for retirees
+function utility_grid_ret(prim::primitives, res::results_trans)
+    @unpack e, γ, R, δ, σ, a_grid, T, na  = prim
+    @unpack r, b = res
 
-    u_r
+    ugrid_ret = zeros(na, na, T) #one for each a and a' and t
+
+    for t = 1:T, a_index=1:na, ap_index=1:na
+        a, ap = a_grid[a_index], a_grid[ap_index]
+        c = (1+r[t])*a + b[t] - ap
+        if c>0
+            ugrid_ret[a_index, ap_index, t] = c^((1-σ)*γ)/(1-σ)
+        else
+            ugrid_ret[a_index, ap_index, t] = -Inf
+        end
+    end
+    ugrid_ret
 end
 
-# Finds utility of workers
-function UtilityWorker(c::Float64, l::Float64, σ::Int64, γ::Float64)
-    if c > 0
-        u_w = (((c^γ)*(1-l)^(1-γ))^(1-σ))/(1-σ) # CRRA utility for worker
-    else
-        u_w = -Inf
-    end
+#utility grid for workers
+function utility_grid_work(prim::primitives,  res::results_trans)
+    @unpack N, R, σ, β, π, γ, e, z, a_grid, na, t_noss, θ, T  = prim
+    @unpack w, r, b, θ_path = res
 
-    u_w
+    ugrid_work = zeros(na*2, na, R, T) #current a and z x choice of ap x age x time
+    # 4 dimension u grid, to keep track of age until retirement (R) and time
+
+    #loop over time, current a and z, age, and  a'
+    for t = 1:T, a_index = 1:na, age = 1:(R-1), ap_index = 1:na, z_index = 1:2
+        a = a_grid[a_index]
+        ap = a_grid[ap_index]
+        labor = getLabor(a_index, z_index, ap_index, prim, res, t, age) #use optimal labor function
+        c = w[t]*(1-θ_path[t])*e[age, z_index]*labor+(1+r[t])*a - ap
+
+        if c>0
+            inside=(c^γ)*(1-labor)^(1-γ)
+            ugrid_work[a_index+na*(z_index - 1), ap_index, age, t] = (inside^(1-σ))/(1-σ)
+        else
+            ugrid_work[a_index + na*(z_index - 1), ap_index, age, t] = -Inf
+        end
+    end
+    ugrid_work #deliverable
 end
 
 # Bellman function for retirees - let make age an argument of the function - maybe it will make the code faster
-function bellman_retiree(prim::primitives, ins::inputs, res::results, t::Int64, age::Int64)
-    @unpack N, R, σ, β, π, γ, e, z = prim
-    @unpack a_grid, na, t_noss, θ  = ins
+function bellman_retiree(prim::primitives, res::results_trans, t::Int64, age::Int64, ugrid_ret::Array{Float64,3})
+    @unpack N, R, σ, β, π, γ, e, z, a_grid, na, t_noss, θ  = prim
     @unpack w, r, b, pol_func, val_func = res
 
 
     value_temp = zeros(na, 2)      # value function guess
 
-#    for t = T:-1:1
-    #    println("Period t = $t")
-    wage = w[t]
-    rate = r[t]
-    ben = b[t]
 
         for z_index = 1:2, a_index = 1:na
-            a = a_grid[a_index]
             max_val = -Inf
-        #    for age = N:-1:R
-                if age == N
-                #    for a_index in 1:na
-                        c = (1 + rate) * a + ben
-                        pol_func[t, a_index, z_index, N] = 0.0
-
-                        if c > 0
-                            max_val = UtilityRetiree(c, σ, γ)
-                        end
-                #    end
-                else
-                    choice_lower = 1                                # for exploiting monotonicity of policy function
-
-                #    for a_index in 1:na
-                        for ap_index in choice_lower:na
-                            a_prime = a_grid[ap_index]
-                            c = (1 + rate) * a + ben - a_prime
-
-                            if c > 0
-                                val = UtilityRetiree(c, σ, γ) + β*val_func[t+1, ap_index, z_index, age+1]
-                                if val > max_val
-                                    max_val = val
-                                    pol_func[t, a_index, z_index, age] = a_prime
-                                    choice_lower = ap_index
-                                end
-                            end
-                        end
+            if age == N
+                max_val = ugrid_ret[a_index, 1, t]
+            else
+                for ap_index in 1:na
+                    val = ugrid_ret[a_index, ap_index, t] + β*val_func[t+1, ap_index, z_index, age+1]
+                    if val > max_val
+                        max_val = val
+                        pol_func[t, a_index, z_index, age] = a_grid[ap_index]
+                    end
                 end
-                value_temp[a_index, z_index] = max_val
-            #end
+            end
+            value_temp[a_index, z_index] = max_val
         end
-    #end
-
     return value_temp
 end
 
 # Bellman function for workers
-function bellman_worker(prim::primitives, ins::inputs, res::results, t::Int64, age::Int64)
-    @unpack N, R, σ, β, π, γ, e, z, = prim
-    @unpack a_grid, na, T, t_noss, θ = ins
+function bellman_worker(prim::primitives, res::results_trans, t::Int64, age::Int64, ugrid_work::Array{Float64,4})
+    @unpack N, R, σ, β, π, γ, e, z, a_grid, na, T, t_noss, θ  = prim
     @unpack w, r, b, pol_func, labor, val_func = res
 
 
     value_temp = zeros(na, 2)      # value function guess
 
-#    for t = T:-1:1
-    #    println("Period t = $t")
-    wage = w[t]
-    rate = r[t]
-    ben = b[t]
+    for z_index = 1:2, a_index = 1:na
+        max_val = -Inf
 
-
-#    for t = T:-1:1
-    #    println("Period t = $t")
-
-        for z_index = 1:2, a_index = 1:na
-        #    for age = R-1:-1:1
-            #    println("Age  = $age")
-
-                choice_lower = 1                                                # for exploiting monotonicity of policy function
-
-            #    for a_index in 1:na
-                    a = a_grid[a_index]
-                    max_val = -Inf
-
-                    for ap_index in choice_lower:na
-                        a_prime = a_grid[ap_index]
-                        l = getLabor(a_index, z_index, ap_index, prim, ins, t, age)
-
-                        c = wage * (1-θ) * e[age, z_index] * l + (1+rate) * a - a_prime
-
-                        if c > 0
-                            val = UtilityWorker(c, l, σ, γ) + β*(val_func[t+1, ap_index, 1, age+1]*π[z_index,1] + val_func[t+1, ap_index, 2, age+1]*π[z_index,2])
-
-
-                            if val > max_val
-                                max_val = val
-                                pol_func[t, a_index, z_index, age] = a_prime
-                                labor[t, a_index, z_index, age] = l
-                                choice_lower = ap_index
-                            end
-                        end
-                    end
-
-                    value_temp[a_index, z_index] = max_val
-                #end
-            #end
+        for ap_index in 1:na
+            val = ugrid_work[a_index + na*(z_index-1), ap_index, age, t]+β*sum(π[z_index,:].*val_func[t+1, ap_index, :, age+1])
+            if val > max_val
+                max_val = val
+                pol_func[t, a_index, z_index, age] = a_grid[ap_index]
+                labor[t, a_index, z_index, age] = getLabor(a_index, z_index, ap_index, prim, res, t, age)
+            end
+        end
+        value_temp[a_index, z_index] = max_val
         end
 
     return value_temp
 end
 
 #backward induction protocol
-function backward_induct(prim::primitives, ins::inputs, res::results, t::Int64)
-    @unpack R, N = prim
-    @unpack T = ins
+function backward_induct(prim::primitives, res::results_trans, t::Int64, ugrid_work::Array{Float64,4}, ugrid_ret::Array{Float64,3})
+    @unpack R, N, T = prim
 
     #loop over all ages
     for j = 1:N
         age = N-j+1 #now going backwards = 1 when j = J
         if age>= R
-            res.val_func[t,:,:,age] = bellman_retiree(prim, ins, res, t, age)
+            res.val_func[t,:,:,age] = bellman_retiree(prim, res, t, age, ugrid_ret)
         else
-            res.val_func[t,:,:,age] = bellman_worker(prim, ins, res, t, age)
+            res.val_func[t,:,:,age] = bellman_worker(prim, res, t, age, ugrid_work)
         end
     end
 end
 
 # Create population distribution by age
-function AgeDistribution(N::Int64, n::Float64)
+function AgeDistribution(prim::primitives)
+    @unpack N, n = prim
+
     μ = ones(N)
     μ[1] = 1
 
-    for i in 2:N
+    for i = 2:N
         μ[i] = μ[i-1]/(1+n)
     end
     μ = μ ./sum(μ)
-    μ
+    return μ
 end
 
 # Generate stationary distribution
-function get_ss_distr(prim::primitives, ins::inputs, res::results, initial_distr::Array{Float64})
-    @unpack π, π0, N, n = prim
-    @unpack na, a_grid, T = ins
+function get_ss_distr(prim::primitives, res::results_trans, initial_distr::Array{Any, 3})
+    @unpack π, π0, N, n, na, a_grid, T = prim
     @unpack pol_func = res
 
     distr = zeros(T, na, 2, N)
     distr[1, :, :, :] = initial_distr
 
-    for t = 2:T
+    for t = 1:(T-1)
         distr[t, 1, :, 1] = prim.μ_1 .* π0  # we know what they should start with at age = 1
-        for age = 2:N
+        for age = 1:(N-1)
             for z_index = 1:2, a_index = 1:na
-                    ap = pol_func[t-1, a_index, z_index, age-1]
+                    ap = pol_func[t, a_index, z_index, age]
                     for ap_index = 1:na, zp_index = 1:2 #loop over tomorrow's states
                         if a_grid[ap_index] == ap
-                            distr[t, ap_index, zp_index, age] += distr[t-1, a_index, z_index, age-1]*
+                            distr[t+1, ap_index, zp_index, age+1] += distr[t, a_index, z_index, age]*
                                 π[z_index, zp_index]*(1/(1+n))
                         end
                     end
@@ -246,10 +167,9 @@ function get_ss_distr(prim::primitives, ins::inputs, res::results, initial_distr
     res.Γ = distr
 end
 
-function CalcAggregate(prim::primitives, ins::inputs, res::results)
-     @unpack R, N, e, z = prim
+function CalcAggregate(prim::primitives, res::results_trans)
+     @unpack R, N, e,  T, na, a_grid  = prim
      @unpack Γ, labor, K, L = res
-     @unpack T, na, a_grid = ins
 
 
      K1 = zeros(T)
@@ -262,10 +182,10 @@ function CalcAggregate(prim::primitives, ins::inputs, res::results)
      # Calculate aggregate labor demand (L1)
     for t = 2:T
         for age = 1:N
-            for a_index = 1:na, z_index = 1:2
-                K1[t] += Γ[t, a_index, z_index, age]*a_grid[a_index] #agg HH savings
-                if age<R
-                    L1[t] += Γ[t, a_index, z_index, age]*e[age, z_index]*labor[t, a_index, z_index, age]
+            for z_index = 1:2, a_index = 1:na
+                K1[t] += Γ[t, a_index, z_index, age]*a_grid[a_index]
+                if age < R
+                    L1[t] += Γ[t, a_index, z_index, age] * e[age, z_index] * labor[t, a_index, z_index, age]
                 end
             end
         end
@@ -274,67 +194,133 @@ function CalcAggregate(prim::primitives, ins::inputs, res::results)
     return K1, L1
 end
 
+function update_prices(prim::primitives, res::results_trans; K0::Array{Float64}, L0::Array{Float64}, μ::Array{Float64})
+    @unpack α, δ, R, N, n, θ, T, t_noss = prim
+    #@unpack K, L  = res
+
+    # Path of wages w
+    for t = 1:T
+        res.w[t] = (1-α) * (K0[t]^α) * (L0[t]^(-α))
+
+        # Path of interest rates r
+        res.r[t] = α * (K0[t]^(α-1))*(L0[t]^(1-α)) - δ
+    end
+
+    # Path of pensions
+    for t = 1:T
+        if t < t_noss
+            @unpack w = res
+            res.θ_path[t] = θ
+            res.b[t] = (θ*w[t]*L0[t])/ sum(μ[R:N])
+        else
+            res.θ_path[t] = 0
+            res.b[t] = 0
+        end
+    end
+    return res.w, res.r, res.b, res.θ_path
+end
+
+
 ### solveModel
-function solveModel(prim::primitives, ins::inputs, res::results; tol = 0.001, iter = 1000, λ = 0.95)
-    @unpack N, R, σ, β, π, γ, e, z, = prim
-    @unpack a_grid, na, T, t_noss, θ = ins
-    @unpack labor, pol_func, val_func, K, L = res
+function solveModel(res_old::results, res_new::results; al = 0.0, au = 20.0, na = 200, θ = 0.11, z = [3.0, 0.5], γ = 0.42, T = 1, t_noss = 2, tol = 0.01, iter = 1000, λ = 0.90)
+    prim, res  = Initialize_trans(al, au, na, θ, z, γ, T, t_noss)
+
+    @unpack N, R, σ, β, π, γ, e, z, a_grid, na, T, t_noss, θ = prim
+    @unpack labor, pol_func, val_func, K, L, w, r, b, θ_path, Γ = res
+
+
+    K[1] = res_old.K
+    K[T] = res_new.K
+    L[1] = res_old.L
+    L[T] = res_new.L
+
+    K_step = (K[T] - K[1])/(T-1)
+    L_step = (L[T] - L[1])/(T-1)
+    for i = 2:T-1
+        K[i] = K[i-1] + K_step
+        L[i] = L[i-1] + L_step
+    end
 
     # Initial guesses for capital and labor paths along the transition
-    K0, L0 = initialGuesses(ins, res)
+#    println("K = $K, and L = $L\n")
+
+
+    # Population shares
+    μ = AgeDistribution(prim)
+#    println("mu = $μ")
+
+    # find prices
+    w, r, b, θ_path = update_prices(prim, res; K0 = K, L0 =  L, μ = μ)
+
+    #fill in the terminal value function, stat distributions, policy functions
+    res.val_func[T, :,:,:] = res_new.val_func
+    res.pol_func[T, :,:,:] = res_new.pol_func
+    res.labor[T, :,:,:] = res_new.labor
+    res.Γ[T,:,:,:] = res_new.Γ
 
     dif = 100
     n_iter = 1
 
     # Solve the agent's problem
     while (dif > tol && n_iter < iter)
+        println("Iter = $n_iter")
 
-        # Prices paths
-        w, r, b = prices(prim, ins, res; Kg = K0, Lg = L0)
-        println("Prices are\n wage = $w,\n rate $r, \n and benefits are $b")
+    #    println("Prices are\n wage = $w,\n rate $r, \n and benefits are $b")
+        ugrid_work = utility_grid_work(prim,  res)
+        ugrid_ret = utility_grid_ret(prim,  res)
+    #    println("grids are filled in\n")
+
 
         for t = (T-1):-1:1 ### do not include the last period since it is already known
-            println("period t = $t")
-            wage = w[t]
-            rate = r[t]
-            ben = b[t]
-
-            backward_induct(prim, ins, res, t)
+    #        println("period t = $t")
+            backward_induct(prim,  res, t, ugrid_work, ugrid_ret)
         end
 
         # Find stationary distribution
-        get_ss_distr(prim, ins, res, res.Γ[1, :, :, :])
+        get_ss_distr(prim, res, res_old.Γ)
         # Find aggregate variables according to the obtained decision rule and distribution
-        K1, L1 = CalcAggregate(prim, ins, res)
+        K1, L1 = CalcAggregate(prim, res)
+    #    println("K1 = $K1 and\n L1 = $L1")
 
         # Compare
-        max_dif_K = maximum(abs.(K1 .- K0))
-        max_dif_L = maximum(abs.(L1 .- L0))
-        println("Max dif K is $max_dif_K and max dif L is $max_dif_L") # ... K DIFFERENCES $(K1 - K0), L DIFFERENCES $(L1 - L0)\n")
+        max_dif_K = maximum(abs.(K1 .- K))
+        max_dif_L = maximum(abs.(L1 .- L))
+    #    println("Max dif K is $max_dif_K and max dif L is $max_dif_L") # ... K DIFFERENCES $(K1 - K0), L DIFFERENCES $(L1 - L0)\n")
 
 
         dif = max_dif_K + max_dif_L
         println("FINDS DIFFERENCE $dif") # ... K DIFFERENCES $(K1 - K0), L DIFFERENCES $(L1 - L0)\n")
-        println("K0 = $K0,\n K1 = $K1, \n L0 = $L0, \n L1 = $L1")
+    #    println("K0 = $K0,\n K1 = $K1, \n L0 = $L0, \n L1 = $L1")
 
         ### We should not change the starting point!
-        K1[1] = K0[1]
-        L1[1] = L0[1]
+        K1[1] = K[1]
+        L1[1] = L[1]
 
+        #for t = 1:T
+        #    if abs(K1[t] - K[t]) > tol
+        #        K[t] = (1-λ)*K[t] + λ*K1[t]
+        #    end
+        #    if abs(L1[t] - L[t]) > tol
+        #        L[t] = (1-λ)*L[t] + λ*L1[t]
+        #    end
+        #end
         if dif > tol
-            K0 = λ*K0 + (1-λ)*K1
-            L0 = λ*L0 + (1-λ)*L1
-        else
+            K = (1-λ)*K1 + λ*K
+            L = (1-λ)*L1 + λ*L
+        end
+
+        res.w, res.r, res.b, res.θ_path = update_prices(prim, res;  K0 = K, L0 =  L, μ = μ)
+        n_iter += 1
+
+        if dif < tol
             res.K = K1
             res.L = L1
             println("DONE!\n")
             break
         end
-
-        n_iter += 1
     end
 
     ##############
 
-    return ins, res     # what we need in order to make graphs
+    return prim, res, K, L     # what we need in order to make graphs
 end
