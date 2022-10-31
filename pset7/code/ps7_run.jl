539833f9da148fc9c3acd3be1b0b2cf73af9e532
Анна Лукьянова <lukianova@wisc.edu>
2022-10-31 15:28:37 -0500

    do not use solve_model function

--- pset7/code/ps7_run.jl
+++ pset7/code/ps7_run.jl
@@ -17,14 +17,14 @@ if pwd() == "C:\\Users\\79267"
     cd("C:\\Users\\79267\\Documents\\UW_PhD\\3rd_year\\computationalF22")
 end
 
-root     = joinpath(pwd(), "pset6")
+root     = joinpath(pwd(), "pset7")
 codepath = joinpath(root, "code")
 
 # import packages used to run the model
 using Parameters, DataFrames
 
 # import model functions
-include("ps6_model.jl")
+include("ps7_model.jl")
 
 
 #======================================================#
@@ -32,25 +32,51 @@ include("ps6_model.jl")
 #------------------------------------------------------#
 # Run model
 
-# Task 1.
-# Baseline
-@time par1, res1 = solve_model(; cf = 10, α = 0)
-#res1.p
-#res1.pol_func
+# Task 1. Derivations - see write up.
 
-# With shocks. α = 1
-@time par2, res2 = solve_model(; cf = 10, α = 1)
-#res2.p
-#res2.pol_func
+### Case 1. Just identified case with 2 moments: mean and variance.
+# Task 2. Simulate the true data.
 
-# With shocks. α = 2
-@time par3, res3 = solve_model(; cf = 10, α = 2)
-#res3.p
-#res3.pol_func
+# Need to run for each type
+
+par, res = Initialize(; type = 2)
+res.y = true_data(par, res)
+
+# Task 3. Sumulate errors for simulations.
+res.ε_grid = errors_sim(par, res)
+
+# Task 4.
+# (a) Plot and consistent estimate b1 with W = I
+res.W = Diagonal(ones(par.n))
+
+# Plot
+x_ax, y_ax, z_ax = plot_J(par, res; ρl = 0.35, ρstep = 0.05, ρh = 0.65, σl = 0.8, σstep = 0.05, σh = 1.2)
+Plots.plot(x_ax, y_ax, z_ax, st=:surface, camera=(-15, 15))
+
+# Estimates
+sol = optimize(J, [0.5, 1.0])
+res.b11, res.b21 = sol.minimizer[1], sol.minimizer[2]
+
+# (b) Find W* and efficient estimate b2 with W = W*
+dif = dif_for_gamma(par, res)
+Γ0, Γj = gamma(par, res; dif = dif)
+S_hat = S(par, res; Γ0 = Γ0, Γj = Γj)
+res.W = inv(S_hat)
+
+# Estimates
+sol2 = optimize(J, [0.5, 1.0])
+res.b12, res.b22 = sol2.minimizer[1], sol2.minimizer[2]
+
+# (c) Derivatives and standard errors
+∇ = derivatives(par, res)
+errors = st_errors(par; ∇ = ∇, S_hat = S_hat)
+
+# (d) J-test WRONG
+j_st = J_test(par)
+
+# Task 5.
+### Case 2. Just identified case with 2 moments: variance and autocovariance.
 
 
-# Task 2
-
-
-# Produce output table
-write_table(output)
+# Task 6.
+### Case 3. Over-identified case with 3 moments: mean, variance and covariance.
